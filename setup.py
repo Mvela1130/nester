e8f180403161a4337aa8f600c35d7b8ab42fb972
Mvela1130 <92660077+Mvela1130@users.noreply.github.com>
2022-01-17 15:23:01 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'nester',
+        version      = '1.1.0',
+        py_modules   = ['nester'],
+        author       = 'Martin Velazquez',
+        author_email = 'martin.velazquez@genesys-aerosystems.com',
+        url          = 'H:\Python1\IDLE\nester',
+        description  = 'A simple nested list of movies',
+    )
